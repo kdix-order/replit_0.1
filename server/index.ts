0be768eb324e2b98f9858f7804cd4b217c1db2b1
Yuto Terada <lugongdao07@gmail.com>
2025-04-16 02:34:56 +0900

    fix: macOSでは5000ポートがシステムに割り当てられることがあるので、3000ポートを使うようにした - ついでにlocalhostからアクセスするようにした

--- server/index.ts
+++ server/index.ts
@@ -56,15 +56,11 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
+  // ALWAYS serve the app on port 3000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "127.0.0.1",
-    // reusePort: true, // このオプションはローカル環境では必要ないため、コメントアウト
-  }, () => {
+  const port = 3000;
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
