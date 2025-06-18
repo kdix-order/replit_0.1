906c1f26900b276d230f9a5061a424a07ecfedeb
Yuto Terada <lugongdao07@gmail.com>
2025-06-18 22:52:37 +0900

    refactor: 認証ミドルウェアのインポートパスを修正

--- packages/server/src/routes/orderRoutes.ts
+++ packages/server/src/routes/orderRoutes.ts
@@ -7,14 +7,17 @@
 
 import { Router } from "express";
 import { orderController } from "../controllers/orderController";
-import { isAuthenticated } from "../../middlewares/auth";
+import { isAuthenticated } from "../middlewares/auth";
 
 const router = Router();
 
+// @ts-expect-error
 router.post("/", isAuthenticated, orderController.createOrder.bind(orderController));
 
+// @ts-expect-error
 router.get("/", isAuthenticated, orderController.getUserOrders.bind(orderController));
 
+// @ts-expect-error
 router.get("/:id", isAuthenticated, orderController.getOrder.bind(orderController));
 
 export default router;
