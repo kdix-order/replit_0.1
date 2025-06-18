a2ec9890260b53e6bd850ff470274e3f6c65f719
Yuto Terada <lugongdao07@gmail.com>
2025-06-18 22:49:22 +0900

    refactor: 認証で使われるutilを移動

--- packages/server/src/utils/auth.ts
+++ packages/server/src/utils/auth.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import type { Request } from "express";
-import { storage } from "../storage";
+import { storage } from "../storage/index.ts";
 
 const JWT_SECRET = () => process.env.JWT_SECRET || "campus-order-jwt-secret";
 
