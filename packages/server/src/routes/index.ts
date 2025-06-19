32e99d3bfb3f5a906c940dcecad01fd2acf2ea88
Yuto Terada <lugongdao07@gmail.com>
2025-06-19 19:30:03 +0900

    feat: 時間枠関連のエンドポイントを追加し、PayPayエンドポイントのパスを修正

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -13,6 +13,7 @@ import cartRoutes from "./cartRoutes";
 import authRoutes from "./authRoutes";
 import adminRoutes from "./adminRoutes";
 import receiptRoutes from "./receiptRoutes";
+import timeslotRoutes from "./timeslotRoutes";
 
 const router = Router();
 
@@ -23,6 +24,7 @@ router.use("/orders", orderRoutes);
 router.use("/payments", paymentRoutes);
 router.use("/products", productRoutes);
 router.use("/receipts", receiptRoutes);
+router.use("/timeslots", timeslotRoutes);
 
 router.post("/idp", (req, res) => {
     console.log("IDP request received:", JSON.stringify(req.body, null, 2));
